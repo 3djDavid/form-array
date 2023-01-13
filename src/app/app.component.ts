0a2401f25d6cf1e75d86707a622ac40734776056
David Vivanco <desarrollowebdavid@gmail.com>
2023-01-12 22:12:09 -0300

    mat-table form-array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+import { MatTableDataSource } from '@angular/material/table'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'form-array';
+
+  fg!: FormGroup
+  dataSourcePacks!: MatTableDataSource<any>;
+  displayedColumns = ["cantDesde", "precio", "eliminar"]
+
+  cantDesde = new FormControl('')
+  precio = new FormControl('')
+
+  constructor(private _fb: FormBuilder,
+    private cd: ChangeDetectorRef) {
+
+  }
+
+  ngOnInit(): void {
+
+    this.fg = this._fb.group({
+      cantDesde: this.cantDesde,
+      precio: this.precio,
+      promos: this._fb.array([])
+    });
+
+  };
+
+  get promos() {
+    return this.fg.controls["promos"] as FormArray;
+  };
+
+  addLesson(): void {
+
+    const lessonForm = this._fb.group({
+      cantDesde: [''],
+      precio: ['']
+    });
+
+
+    this.promos.push(lessonForm);
+    this.dataSourcePacks = new MatTableDataSource(this.promos.controls);
+
+    this.cd.detectChanges();
+
+  };
+
+
+  deleteLesson(lessonIndex: number): void {
+
+    this.promos.removeAt(lessonIndex);
+    this.dataSourcePacks = new MatTableDataSource(this.promos.controls);
+
+  };
+
+
+  onSubmit() {
+    console.log(this.promos.value)
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    ReactiveFormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
